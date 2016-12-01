0addb2f2932f89ef91fa00dafa9cea3337562702
Albert Pak <al@albert-pak.com>
2016-12-01 09:59:14 -0600

    alert entered input

--- public/app.jsx
+++ public/app.jsx
@@ -5,6 +5,14 @@ var Greeter = React.createClass({
     };
   },
 
+  onButtonSubmit: function () {
+    e.preventDefault();
+
+    var name = this.refs.name.value;
+
+    alert(name);
+  },
+
   render: function () {
     var name = this.props.name;
 
@@ -12,12 +20,19 @@ var Greeter = React.createClass({
       <div>
         <h1>Hello {name}</h1>
         <p>This is from a component</p>
+
+        <form onSubmit={this.onButtonSubmit}>
+          <input type="text" ref="name" />
+          <button>Set Name</button>
+        </form>
       </div>
     );
   },
 });
 
+var firstName = 'Al';
+
 ReactDOM.render(
-  <Greeter />,
+  <Greeter naem={firstName} message="Hello from a component" />,
   document.getElementById('app')
 );
